11ecd47301727e428d3885937336f476271e7dd9
nikitasontake <101354370+nikitasontake@users.noreply.github.com>
2025-03-27 09:42:10 +0530

    Added Changes

--- java/Assignment4.java
+++ java/Assignment4.java
@@ -1,11 +1,12 @@
 public class Assignment4 {
     public static void main(String[] args){
+        System.out.println("From Main Method");
         Assignment4 a4 = new Assignment4();
         a4.test();
     }
 
     public void test(){
         System.out.println("IntelliJ first Project");
-        System.out.println("------Welcome to IntiLLiJ------");
+        System.out.println("------From Test Method------");
     }
 }
